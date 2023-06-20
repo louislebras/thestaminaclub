4aedb7b62879719c9b53e6b5ef0442c95a375e40
Louis Le Bras <louislebras@macbook-pro-de-louis.home>
2023-06-20 19:20:01 +0200

    reztrez

--- public/sources/js/burger.js
+++ public/sources/js/burger.js
@@ -1,11 +1,19 @@
 var burger = document.querySelector(".burger");
 var closeCross = document.querySelector(".close-sidebar");
 var nav = document.querySelector(".sidebar");
+var blurBlock = document.querySelector(".back-blur-layer-sidebar");
 
 burger.onclick = function () {
   nav.classList.add("to-left-menu");
+  blurBlock.classList.add("to-back-blur");
 };
 
 closeCross.onclick = function () {
   nav.classList.remove("to-left-menu");
+  blurBlock.classList.remove("to-back-blur");
+};
+
+blurBlock.onclick = function () {
+  nav.classList.remove("to-left-menu");
+  blurBlock.classList.remove("to-back-blur");
 };
