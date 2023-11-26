f75997d596ac1ece359c2af45576c7908e66b42b
Louis Le Bras <louislebras@mbp-de-louis.home>
2023-11-26 12:14:23 +0100

    join

---
+++ public/sources/js/join-header.js
@@ -0,0 +1,37 @@
+var limit = 0.6, //implies 40 vh or 40% of viewport height
+  scrollLimit = 0;
+
+$(window)
+  .resize(function () {
+    scrollLimit = $(window).height() * limit;
+  })
+  .scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= scrollLimit) {
+      $(".logo-fixed-header").addClass("darkHeader");
+    } else {
+      $(".logo-fixed-header").removeClass("darkHeader");
+    }
+  })
+  .trigger("resize")
+  .trigger("scroll"); // trigger both events on start to force initial setup
+
+var limit2 = 0.6, //implies 40 vh or 40% of viewport height
+  scrollLimit = 0;
+
+$(window)
+  .resize(function () {
+    scrollLimit = $(window).height() * limit2;
+  })
+  .scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= scrollLimit) {
+      $(".logo-fixed-join").addClass("hideLogo");
+    } else {
+      $(".logo-fixed-join").removeClass("hideLogo");
+    }
+  })
+  .trigger("resize")
+  .trigger("scroll"); // trigger both events on start to force initial setup
---
+++ public/sources/js/narrative.js
@@ -0,0 +1,42 @@
+var slideOne = document.querySelector(".slide1");
+var nextOne = document.querySelector(".next1");
+
+nextOne.onclick = function () {
+  slideOne.classList.add("tada");
+};
+
+var slideTwo = document.querySelector(".slide2");
+var nextTwo = document.querySelector(".next2");
+
+nextTwo.onclick = function () {
+  slideTwo.classList.add("tada");
+};
+
+var slideThree = document.querySelector(".slide3");
+var nextThree = document.querySelector(".next3");
+
+nextThree.onclick = function () {
+  slideThree.classList.add("tada");
+};
+
+var slideFour = document.querySelector(".slide4");
+var nextFour = document.querySelector(".next4");
+
+nextFour.onclick = function () {
+  slideFour.classList.add("tada");
+};
+
+var slideFive = document.querySelector(".slide5");
+var nextFive = document.querySelector(".next5");
+var boxNarrative = document.querySelector(".narrative");
+
+nextFive.onclick = function () {
+
+  boxNarrative.classList.add("tada2");
+};
+
+var skipButton = document.querySelector(".skip-button");
+
+skipButton.onclick = function () {
+  boxNarrative.classList.add("tada2");
+};
